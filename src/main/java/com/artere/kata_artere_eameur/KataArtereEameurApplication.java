4a1918f5c09d03a650a4177bba9b819729ba122f
ELMEHDI <elmehdi.ameur97@gmail.com>
2025-02-03 22:49:10 +0100

    git init project

---
+++ src/main/java/com/artere/kata_artere_eameur/KataArtereEameurApplication.java
@@ -0,0 +1,13 @@
+package com.artere.kata_artere_eameur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataArtereEameurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataArtereEameurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artere/kata_artere_eameur/KataArtereEameurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artere.kata_artere_eameur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataArtereEameurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
